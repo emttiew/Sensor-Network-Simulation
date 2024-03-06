74a8b397ea56d5305427f2e5e9fef6af6930b93a
Mateusz Wozniak <emttiew@gmail.com>
2024-03-06 16:53:21 +0100

    Notify clients from the MainNode after 1 second delay

--- src/MainNode.hpp
+++ src/MainNode.hpp
@@ -1,50 +1,55 @@
 #pragma once
 
 #include <boost/circular_buffer.hpp>
+#include <condition_variable>
 #include <mutex>
 #include <thread>
-#include <condition_variable>
 
 #include "Client.hpp"
 
 class MainNode {
-public:
-    explicit MainNode(int bufferSize) : buffer(bufferSize) {}
+ public:
+  explicit MainNode(int bufferSize) : buffer(bufferSize) {}
 
-    void receiveData(int data) {
-        std::lock_guard<std::mutex> lock(mutex);
+  void receiveData(int data) {
+    std::lock_guard<std::mutex> lock(mutex);
 
-        buffer.push_back(data);
-        condVar.notify_one();
+    buffer.push_back(data);
+
+    // Notify clients if it's time
+    auto now = std::chrono::system_clock::now();
+    if (now - lastNotificationTime >= std::chrono::seconds(1)) {
+      condVar.notify_one();
+      lastNotificationTime = now;
     }
+  }
 
-    void start() {
-        std::thread notifyThread(&MainNode::notifyClients, this);
-        notifyThread.detach();
-    }
+  void start() {
+    std::thread notifyThread(&MainNode::notifyClients, this);
+    notifyThread.detach();
+  }
 
-     void addClient(Client& client) {
-        clients.push_back(client);
-    }
+  void addClient(Client& client) { clients.push_back(client); }
 
-private:
-    boost::circular_buffer<int> buffer;
-    std::mutex mutex;
-    std::condition_variable condVar;
-    std::vector<std::reference_wrapper<Client>> clients;
+ private:
+  boost::circular_buffer<int> buffer;
+  std::mutex mutex;
+  std::condition_variable condVar;
+  std::vector<std::reference_wrapper<Client>> clients;
+  std::chrono::system_clock::time_point lastNotificationTime;
 
-    void notifyClients() {
-        while (true) {
-            std::unique_lock<std::mutex> lock(mutex);
+  void notifyClients() {
+    while (true) {
+      std::unique_lock<std::mutex> lock(mutex);
 
-            condVar.wait(lock);
+      condVar.wait(lock);
 
-            for (int data : buffer) {
-                for (Client& client : clients) {
-                    client.receiveData(data);
-                }
-            }
-            buffer.clear();
+      for (int data : buffer) {
+        for (Client& client : clients) {
+          client.receiveData(data);
         }
+      }
+      buffer.clear();
     }
+  }
 };
